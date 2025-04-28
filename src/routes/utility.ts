68753217ff5330a82106c83a66f76f8398b5253d
Whitzscott <whitzscott@gmail.com>
2025-04-28 10:51:20 +0800

    Added a check inside utilitiy

--- src/routes/utility.ts
+++ src/routes/utility.ts
@@ -8,3 +8,9 @@ export const SUPABASE_URL = "https://cqtbishpefnfvaxheyqu.supabase.co";
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY || !JWT_SECRET || !OPENROUTER_API_KEY || !OPENAI_API_KEY) {
+    console.log(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+    throw new Error('Missing Supabase URL or Service Role Key in environment variables.');
+}
+  
